c4ab85c6e8d16b55a8c52780aa6913c12129cbe4
alebra64 <alexbr64@gmail.com>
2025-05-03 22:22:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 // Redireziona l’utente all’endpoint di autorizzazione di Zoom
 
 $client_id = 'D9SL3ztxScWLjlPeCBFzBA'; // Il tuo Client ID
-$redirect_uri = 'https://zoomoodle-ut.herokuapp.com/callback.php'; // Questo è l’URL registrato su Zoom
+$redirect_uri = 'https://zomoodle-8977176c3197.herokuapp.com/callback.php';  // Questo è l’URL registrato su Zoom
 $scope = 'meeting:read:admin webinar:read:admin user:read:admin user:write:admin';
 
 $authorize_url = "https://zoom.us/oauth/authorize?response_type=code"
